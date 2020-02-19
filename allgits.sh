8fceac7336b8cb47d3e6fe5edec1d5c20391539b
Max Winokan <mwinokan@me.com>
2020-02-19 10:40:25 +0000

    Added -s flag for shorter output.

--- allgits.sh
+++ allgits.sh
@@ -4,6 +4,20 @@
 # Works on EUREKA
 
 source $MWSHPATH/colours.sh
+short=0
+
+while test $# -gt 0; do
+  case "$1" in
+    -s|--short)
+      shift
+      SHORT=1
+      ;;
+    *)
+      echo -e $colError"Unknown flag: "$colArg$1$colClear
+      exit 1
+      ;;
+  esac
+done
 
 ALL_GITS=$(find $HOME -name ".git" | sort)
 
@@ -14,7 +28,8 @@ GL_STAFFNAME=$(grep -oP "(?<=staffname=).*(?=;)" $MWSHPATH/.suppressed_gitlab)
 
 for GIT in $ALL_GITS ; do
 
-  REPO_NAME=$(cat $GIT/config | grep -oP "(?<=/).*(?=.git)")
+  # grep -P "(?<=$GH_USER/).*(?=.git)" $GIT/config
+  REPO_NAME=$(cat $GIT/config | grep -oP "(?<=$GH_USER/).*(?=.git)")
   if [ $(cat $GIT/config | grep "github" | wc -l) -eq 1 ] ; then
     ## github repo
     if [ $(cat $GIT/config | grep $GH_USER | wc -l) -eq 1 ] ; then
@@ -30,7 +45,7 @@ for GIT in $ALL_GITS ; do
     ## gitlab repo
     if [ $(cat $GIT/config | grep $GL_STAFFNAME | wc -l) -eq 1 ] ; then
       ## my repo
-      REPO_NAME=$(cat $GIT/config | grep -oP "(?<=$USERCODE/).*(?=.git)")
+      REPO_NAME=$(cat $GIT/config | grep -oP "(?<=$GL_USERCODE/).*(?=.git)")
       REPO_TYPE=2
     else
       ## not my repo
@@ -53,15 +68,15 @@ for GIT in $ALL_GITS ; do
   NAME_LINE='                                           '
   # NAME_LINE='-------------------------------------------'
   
-  echo -e -n "Repo: "
+  if [ $SHORT -eq 0 ] ; then echo -e -n "Repo: "; fi
   # echo -e -n $colFunc$REPO_NAME$colClear" "
   if [ $REPO_TYPE -eq 1 ] ; then echo -e -n "(github) " ; fi
   if [ $REPO_TYPE -eq 2 ] ; then echo -e -n "(gitlab) " ; fi
   printf "$colFunc%s$colClear %s" $REPO_NAME "${NAME_LINE:${#REPO_NAME}}"
   if [ $STATUS -eq 0 ] ; then echo -e -n $colSuccess"No Modifications"$colClear ; fi
   if [ $STATUS -eq 1 ] ; then echo -e -n $colBold$NUM_MOD" Modified Files"$colClear ; fi
-  echo -e -n " Local: "
-  echo -e -n $colFile$GIT$colClear
+  if [ $SHORT -eq 0 ] ; then echo -e -n " Local: "; fi
+  if [ $SHORT -eq 0 ] ; then echo -e -n $colFile$GIT$colClear; fi
 
   if [ $REPO_TYPE -eq 1 ] ; then
 
