5ddbbdf7c446ddaa296301d585ae6b0a5fc3d8ad
mwinokan <mwinokan@me.com>
2020-02-20 21:11:35 +0000

    Clearer output.

--- unmount.sh
+++ unmount.sh
@@ -3,6 +3,16 @@
 source $MWSHPATH/colours.sh
 source $MWSHPATH/out.sh
 
+clear
+
+MOUNT_NUM=$(ls /Volumes | grep -v Macintosh | grep -v TimeMachine | grep -v BOOTCAMP | wc -l)
+if [ $MOUNT_NUM -eq 0 ] ; then 
+  echo
+  headerOut "No volumes mounted."
+  echo
+  exit
+fi
+
 headerOut "Mounted Volumes:"
 ls /Volumes | grep -v Macintosh | grep -v TimeMachine | grep -v BOOTCAMP
 
@@ -11,3 +21,5 @@ osascript -e 'tell application "Finder" to eject (every disk whose ejectable is 
 
 MOUNT_NUM=$(ls /Volumes | grep -v Macintosh | grep -v TimeMachine | grep -v BOOTCAMP | wc -l)
 returnOut "Number of mounted volumes: " $MOUNT_NUM
+
+echo
