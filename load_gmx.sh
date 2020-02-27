f88347aaed4cd1db10acc736e0666582bfc46de1
Max Winokan <mwinokan@me.com>
2020-02-27 11:08:12 +0000

    Load Gromacs modules on EUREKA.

---
+++ load_gmx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+module purge              # Unload all currently loaded modules to reduce chance of conflicts. 
+module load gromacs/2018  # Load the GROMACS module and its dependencies.
+
+source /opt/proprietary-apps/gromacs/2018/bin/GMXRC.bash # Set up the GROMACS environment variables.
