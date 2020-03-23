14421e49140872a28145c5f2cbce67141186604d
Max Winokan <mwinokan@me.com>
2020-03-23 17:00:58 +0000

    Custom force field directory.

--- load_gmx.sh
+++ load_gmx.sh
@@ -4,3 +4,5 @@ module purge              # Unload all currently loaded modules to reduce chance
 module load gromacs/2018  # Load the GROMACS module and its dependencies.
 
 source /opt/proprietary-apps/gromacs/2018/bin/GMXRC.bash # Set up the GROMACS environment variables.
+
+export GMXLIB=$HOME"/gmx_ff"
