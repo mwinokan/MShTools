62eafca29530e219f480a0f27e85cca67df57435
Max Winokan <mwinokan@me.com>
2020-04-20 14:17:21 +0100

    gmx_funcs and load_gmx moved to GROMAX

--- load_gmx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module purge              # Unload all currently loaded modules to reduce chance of conflicts. 
-module load gromacs/2018  # Load the GROMACS module and its dependencies.
-
-source /opt/proprietary-apps/gromacs/2018/bin/GMXRC.bash # Set up the GROMACS environment variables.
-
-export GMXLIB=$HOME"/gmx_ff"
