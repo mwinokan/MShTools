8cc67ef66343ce9b9187b52024880d90f04d16eb
Max Winokan <mwinokan@me.com>
2020-12-07 11:27:28 +0000

    Archer support

--- configure.sh
+++ configure.sh
@@ -21,10 +21,10 @@ while test $# -gt 0; do
       echo "token="$4";" >> .suppressed_gitlab
       exit 0
       ;;
-    --configure-scarf)
+    --configure-extern)
       echo "# Suppressed Information Do Not Distribute" > .suppressed_gitlab
       shift
-      echo "user="$1";" >> .suppressed_scarf
+      echo "user="$1";" >> .suppressed_extern
       exit 0
       ;;
     *)
@@ -32,3 +32,17 @@ while test $# -gt 0; do
       ;;
   esac
 done
+
+echo "MShTools Setup"
+echo "--------------"
+
+echo
+
+echo "This setup is required for many of the scripts in this repository."
+echo "Please run configure with all the necessary credentials."
+
+echo
+
+echo "configure.sh --configure-github <author_name> <user> <email>"
+echo "configure.sh --configure-gitlab <author_name> <surey_usercode> <surrey_staffname> <gitlab_token>"
+echo "configure.sh --configure-extern <username>"
