2131cf1ec16fc51cc8d61905ffbcfbc05005f497
Max <mwinokan>
2023-06-12 17:00:56 +0100

    auto configuration

--- configure.sh
+++ configure.sh
@@ -1,40 +1,131 @@
 #!/bin/bash
 
+function auto_configure {
+
+  # configure github:
+
+  echo "What is your full name?"
+  read FULL_NAME
+
+  echo "Do you want to set-up github credentials? (Y/N)"
+  read CHOICE
+
+  if [ $CHOICE == 'Y' ] ; then
+
+    echo "Enter your Github username:"
+    read GITHUB_USER
+
+    echo "Enter your Github email:"
+    read GITHUB_EMAIL
+
+    configure_github $FULL_NAME $GITHUB_USER $GITHUB_EMAIL
+
+  fi
+
+  # if in an SSH session
+  if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+
+    HOSTNAME=$(hostname -f)
+
+    # if on a surrey cluster
+    if [ $HOSTNAME == *surrey* ]; then
+
+      echo "Detected a Surrey HPC, if this is wrong exit this process with Ctrl-C!!!"
+
+      USER_CODE=$(whoami)
+      echo "Guessed surreycode from whoami as: "$USER_CODE
+      
+      echo "Do you have a staff email? e.g. m.winokan@surrey.ac.uk (Y/N)"
+      read CHOICE
+
+      CODE_EMAIL=$USER_CODE@surrey.ac.uk
+
+      if [ $CHOICE == 'Y' ] ; then
+        echo "Enter your staff email:"
+        read STAFF_EMAIL
+      else
+        STAFF_EMAIL=$CODE_EMAIL
+      fi
+
+      echo "Do you have a Gitlab access token? (Y/N)"
+      read CHOICE
+
+      if [ $CHOICE == 'Y' ] ; then
+        echo "Enter your access token:"
+        read ACCESS_TOKEN
+      else
+        ACCESS_TOKEN="XXX"
+      fi
+
+      configure_gitlab $FULL_NAME $USER_CODE $CODE_EMAIL $STAFF_EMAIL $ACCESS_TOKEN
+
+    else
+
+      echo "Guessed username from whoami as: "$USER_CODE
+      USER_CODE=$(whoami)
+
+      configure_extern $USER_CODE
+
+    fi
+
+  fi
+
+}
+
+function configure_github {
+
+  # Store suppressed information
+  echo "# Suppressed Information Do Not Distribute" > .suppressed_github
+  echo "author="$1";" >> .suppressed_github
+  echo "user="$2";" >> .suppressed_github
+  echo "email="$3";" >> .suppressed_github
+  
+  # Run setups
+  git config --global user.name "$1"
+  git config --global user.email "$3"
+
+}
+
+
+function configure_gitlab {
+
+  echo "# Suppressed Information Do Not Distribute" > .suppressed_gitlab
+  echo "author="$1";" >> .suppressed_gitlab
+  echo "surrey_usercode="$2";" >> .suppressed_gitlab
+  echo "surrey_staffname="$3";" >> .suppressed_gitlab
+  echo "surrey_useremail="$2"@surrey.ac.uk;" >> .suppressed_gitlab
+  echo "surrey_staffemail="$3"@surrey.ac.uk;" >> .suppressed_gitlab
+  echo "token="$4";" >> .suppressed_gitlab
+
+}
+
+function configure_extern {
+
+  echo "# Suppressed Information Do Not Distribute" > .suppressed_gitlab
+  echo "user="$1";" >> .suppressed_extern
+
+}
+
 while test $# -gt 0; do
   case "$1" in
     --configure-github)
-      # Store suppressed information
-      echo "# Suppressed Information Do Not Distribute" > .suppressed_github
       shift
-      echo "author="$1";" >> .suppressed_github
-      echo "user="$2";" >> .suppressed_github
-      echo "email="$3";" >> .suppressed_github
-      
-      # Run setups
-      git config --global user.name "$1"
-      git config --global user.email "$3"
-
+      configure_github $1 $2 $3
       exit 0
       ;;
     --configure-gitlab)
-      echo "# Suppressed Information Do Not Distribute" > .suppressed_gitlab
       shift
-      echo "author="$1";" >> .suppressed_gitlab
-      echo "surrey_usercode="$2";" >> .suppressed_gitlab
-      echo "surrey_staffname="$3";" >> .suppressed_gitlab
-      echo "surrey_useremail="$2"@surrey.ac.uk;" >> .suppressed_gitlab
-      echo "surrey_staffemail="$3"@surrey.ac.uk;" >> .suppressed_gitlab
-      echo "token="$4";" >> .suppressed_gitlab
-
-      # Do some other setups
-
-
+      configure_gitlab $1 $2 $3 $4
       exit 0
       ;;
     --configure-extern)
-      echo "# Suppressed Information Do Not Distribute" > .suppressed_gitlab
       shift
-      echo "user="$1";" >> .suppressed_extern
+      configure_extern $1
+      exit 0
+      ;;
+    --auto)
+      # run auto-configuration
+      auto_configure
       exit 0
       ;;
     *)
@@ -47,12 +138,18 @@ echo "MShTools Setup"
 echo "--------------"
 
 echo
-
 echo "This setup is required for many of the scripts in this repository."
-echo "Please run configure with all the necessary credentials."
+echo
+echo "The easiest way to run this script is to run the command as follows:"
+echo 
+echo "configure.sh --auto"
+echo
 
+echo "If automatic setup fails, you can try setting the relevant details manually:"
 echo
 
 echo "configure.sh --configure-github <author_name> <user> <email>"
 echo "configure.sh --configure-gitlab <author_name> <surey_usercode> <surrey_staffname> <gitlab_token>"
 echo "configure.sh --configure-extern <username>"
+echo
+echo "If problems persist try asking Max."
